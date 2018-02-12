6ede7be51203281e15ca54bb2d9272f72c03509a
ノグソン <nogson@MacBook-Pro-3.local>
2018-02-13 00:46:49 +0900

    エラー発生中

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,38 @@ module.exports = {
   entry: path.resolve(__dirname, './src/main.js'), //ビルドするファイル
   output: {
     path: path.resolve(__dirname, './dist'), //ビルドしたファイルを吐き出す場所
-    filename: 'bundle.js',//ビルドした後のファイル名
+    filename: 'bundle.js', //ビルドした後のファイル名
     publicPath: '/dist'
   },
   module: {
-    loaders: [
-      {
+    loaders: [{
         test: /\.html$/,
         loader: "html-loader"
       },
-      { 
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /node_modules/,
+        loader: 'ify-loader'
+      },
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'raw-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'glslify-loader',
+        exclude: /node_modules/
+      }
+    ],
+    postLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'ify'
       }
     ]
   },
@@ -27,6 +46,6 @@ module.exports = {
   },
   devtool: 'source-map',
   plugins: [
-  
+
   ]
 };
